7205f5a1d376775068b077b5ecb35bc9849a3a85
KevinRichard <kevinrichard@MacBook-Pro.local>
2020-07-16 19:30:39 -0300

    ModelMigrationUsers

--- src/database/migrations/1594239583304-CreateUsers.ts
+++ src/database/migrations/1594239583304-CreateUsers.ts
@@ -1,10 +1,11 @@
-import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export class CreateAppointments1594167150376 implements MigrationInterface {
+export default class CreateUsers1594239583304 implements MigrationInterface {
+
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -14,14 +15,17 @@ export class CreateAppointments1594167150376 implements MigrationInterface {
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
-            isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
-            isNullable: false,
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
@@ -39,6 +43,6 @@ export class CreateAppointments1594167150376 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
