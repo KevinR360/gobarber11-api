607fc7be8abaa2b8f36b1a961e6aba4f1aeefb9a
KevinRichard <kevinrichard@MacBook-Pro.local>
2020-07-21 20:35:44 -0300

    ExceptionsControll

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -10,7 +10,6 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-  try {
     const { name, email, password } = request.body;
 
     const createUser = new CreateUserService();
@@ -20,9 +19,6 @@ usersRouter.post('/', async (request, response) => {
     delete user.password;
 
     return response.json(user);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
 });
 
 usersRouter.patch(
@@ -30,19 +26,14 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const updateUserAvatar = new UpdateUserAvatarService();
+    const updateUserAvatar = new UpdateUserAvatarService();
 
-      const user = await updateUserAvatar.execute({
-        user_id: request.user.id,
-        avatarFilename: request.file.filename,
-      });
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
 
-      return response.json(user);
-
-    } catch (err) {
-      return response.status(400).json({ error: err.message });
-    }
+    return response.json(user);
   },
 );
 
